4718093be6b77d655220e5abb9dd00b5a3649e42
Edu <serranotrenado.e@gmail.com>
2025-05-15 10:57:21 +0200

    added db simulation w/ js and JSON, also added some pet photos

---
+++ db/app.js
@@ -0,0 +1,60 @@
+let reservas = [];
+// fetch cargamos los datos
+fetch('/db/datos.json')
+  .then(res => res.json())
+  .then(data => {
+    reservas = data;
+    renderizarReservas();
+});
+
+//------------------------Trabajamos con el array olvidando el JSON
+// CRUD
+function agregarReserva(datos) {
+  const id = reservas.length ? reservas[reservas.length - 1].id + 1 : 1;
+  reservas.push({ id, ...datos, usuarioReserva: usuarioActual });
+}
+function editarReserva(id, nuevosDatos) {
+  const idx = reservas.findIndex(r => r.id === id);
+  if (idx !== -1) reservas[idx] = { ...reservas[idx], ...nuevosDatos };
+}
+function eliminarReserva(id) {
+  reservas = reservas.filter(r => r.id !== id);
+}
+
+// 3. Renderizado en DOM
+function renderizarReservas() {
+  const row = document
+    .getElementById('seccion-reservas')
+    .querySelector('.row.g-2');
+  row.innerHTML = ''; // limpio
+
+  reservas.forEach(r => {
+    const col = document.createElement('div');
+    col.className = 'col-12 evento-reservado';
+    col.innerHTML = `
+      <div class="card d-grid">
+        <div class="row">
+          <div class="col-1">
+            <h5 class="card-title">${r.nombre}</h5>
+          </div>
+          <div class="contenedor-info col-4">
+            <p>id : ${r.id}</p>
+            <p>Estado : ${r.estado}</p>
+            <p>APM : ${r.apm}</p>
+          </div>
+          <div class="contenedor-botones col-5 d-flex justify-content-center">
+            <button class="me-4 button btn-edit" data-id="${r.id}">Editar</button>
+            <button class="me-4 button btn-delete" data-id="${r.id}">Eliminar</button>
+            <button class="me-4 button btn-view" data-id="${r.id}">Ver detalles</button>
+          </div>
+          <div class="col-2 seccion-imagen">
+            <img src="${r.imagen}" alt="imagen-evento">
+          </div>
+        </div>
+      </div>
+    `;
+    row.appendChild(col);
+  });
+}
+// inicializar render
+renderizarReservas();
