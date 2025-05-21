3d9c158dc7fee32996fc7db8f5ceff45e403b34a
Eduu115 <serranotrenado.e@gmail.com>
2025-05-21 09:15:00 +0200

    separated fecht for distinct filters

--- static/js/vistaEventos.js
+++ static/js/vistaEventos.js
@@ -1,12 +1,57 @@
-let reservas = [];
-// fetch cargamos los datos
+let eventos = [];
+let destacados = [];
+let activosNoD = [];
+let soldOut = [];
+// fetch GENERAL
 fetch('http://localhost:9003/evento/todos')
   .then(res => res.json())
   .then(data => {
-    reservas = data;
+    eventos = data;
 });
 
-const subCol1 = document.getElementById("fila-eventos-destacados").querySelector("col-6 sub-columna1-eventos");
+// fetch filtrado por dest
+fetch('http://localhost:9003/evento/activoAndDestacado/S')
+  .then(res => res.json())
+  .then(data =>{
+    destacados = data;
+  });
+// fetch filtrado por act
+fetch('http://localhost:9003/evento/activoAndDestacado/N')
+  .then(res => res.json())
+  .then(data =>{
+    activosNoD = data;
+  });
+// fetch filtrado por sold-out
+fetch('http://localhost:9003/evento/estado/TERMINADO')
+.then(res => res.json())
+.then(data => {
+  soldOut = data;
+});
+
+// Destacados 1
+function renderEventosD1(){
+
+}
+// Destacados 2
+function renderEventosD2(){
+
+}
+// Activos 1
+function renderEventosA1(){
+
+}
+// Activos 2
+function renderEventosA2(){
+
+}
+// Sold-out 1
+function renderEventosE1(){
+
+}
+// Sold-out 2
+function renderEventosE2(){
+
+}
 
 console.log(reservas);
 
