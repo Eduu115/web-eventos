f9698f9714f58af65e68e15f9581d80e2b155af1
Tony1406 <anthony.go0609@gmail.com>
2025-05-22 14:26:47 +0200

    minorchanges jeje

---
+++ static/js/verReservas.js
@@ -0,0 +1,69 @@
+let reservas = [];
+// fetch cargamos los datos
+fetch('http://localhost:9003/reservas/todos')
+  .then(res => res.json())
+  .then(data => {
+    reservas = data;
+    mostrarReserva();
+});
+
+// 3. Renderizado en DOM
+function mostrarReserva() {
+  const row = document
+    .getElementById('seccion-reservas')
+    .querySelector('.row.g-2');
+  row.innerHTML = ''; // limpio
+
+  reservas.forEach(r => {
+    const col = document.createElement('div');
+    col.className = 'col-12 evento-reservado';
+    col.innerHTML = `
+      <div class="card d-grid">
+        <div class="row">
+          <div class="col-1">
+            <h5 class="card-title">${r.nombre}</h5>
+          </div>
+          <div class="contenedor-info col-4">
+            <p>id : ${r.idEvento}</p>
+            <p>Estado : ${r.estado}</p>
+            <p>APM : ${r.aforoMaximo}</p>
+          </div>
+          <div class="contenedor-botones col-5 d-flex justify-content-center">
+              <button class="me-4 button btn-edit" data-id="${r.idEvento}">Editar</button>
+              <button class="me-4 button btn-delete" data-id="${r.idEvento}">Eliminar</button>
+              <button class="me-4 button btn-view" data-bs-toggle="collapse" data-bs-target="#collapse-${r.idEvento}" aria-expanded="false" aria-controls="collapse-${r.idEvento}">Ver detalles</button>
+          </div>
+          <div class="col-2 seccion-imagen">
+            <img class="imagen-dashboard" src="${r.rutaImagen}" alt="imagen-evento">
+          </div>
+        </div>
+      </div>
+
+      <div class="collapse" id="collapse-${r.idEvento}">
+      <div class="card card-body">
+        <div class="evento-detalles d-flex gap-4 flex-wrap">
+          <div class="detalle-columna flex-fill">
+            <p><strong>Descripción:</strong>
+              <span>${r.descripcion || 'Sin descripción disponible'}</span>
+            </p>
+            <p><strong>Fecha de Inicio:</strong> <span>${r.fechaInicio || '-'}</span></p>
+            <p><strong>Fecha de Alta:</strong> <span>${r.fechaAlta || '-'}</span></p>
+            <p><strong>Duración:</strong> <span>${r.duracion || '-'}</span></p>
+            <p><strong>Dirección:</strong> <span>${r.direccion || '-'}</span></p>
+          </div>
+          <div class="detalle-columna flex-fill">
+            <p><strong>Aforo Máximo:</strong> <span>${r.aforoMaximo || '-'}</span></p>
+            <p><strong>Precio:</strong> <span>${r.precio || '-'}</span></p>
+            <p><strong>Estado:</strong> <span>${r.estado || '-'}</span></p>
+            <p><strong>Destacado:</strong> <span>${r.destacado ? 'Sí' : 'No'}</span></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+  });
+}
+
+
+// inicializar render
+mostrarReserva();
