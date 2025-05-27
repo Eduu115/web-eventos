03e249076806b579ec45eb00f4a3f06f53475390
Eduu115 <serranotrenado.e@gmail.com>
2025-05-27 14:24:02 +0200

    fixed js at delete buton and forEach

--- static/js/misReservas.js
+++ static/js/misReservas.js
@@ -2,35 +2,19 @@
 let reservasUsuario = [];
 // cargamos datos del usuario
 const usuario = JSON.parse(localStorage.getItem('user') || '{}');
-// declaro constantes
-const deleteButton = col.querySelector(".btn-delete");
-row.appendChild(col);
-// ========================= Eventos ==============================
-document.addEventListener("DOMContentLoaded", ()=>{
-  deleteButton.addEventListener("click", () => eliminarReserva(r.idReserva));
-});
-
+const idUsuario = usuario.idUsuario;
 // ========================== FETCH ===============================
 // fetch cargamos los datos de las reservas
-fetch(``, {})
-//trabajamos respuesta
-.then(res => res = res.json())
-// procesamos los datos
-.then(
-  data => {
+fetch(`http://localhost:9003/reserva/usuario/${idUsuario}`)
+.then(res => res = res.json()) //trabajamos respuesta
+.then(data => { // procesamos los datos
     reservasUsuario = data;
-  }
-)
-// trabajamos los datos
-.then(
+    renderizarReservas();
+})
+.catch((err) => console.error("Error al cargar reservas:", err));
 
-  //identifico constantes
-  reservasUsuario.forEach(r =>{
-    
-  })
-  
-)
-.catch();
+
+// ============================================== FUNCIONES ==============================================
 
 function eliminarReserva(idReserva) {
   fetch(`http://localhost:9003/reserva/eliminar/${idReserva}`, {
@@ -41,8 +25,70 @@ function eliminarReserva(idReserva) {
       return res.json();
     })
     .then(() => {
-      reservas = reservas.filter((ev) => ev.idReserva !== idReserva);
+      reservasUsuario = reservasUsuario.filter((ev) => ev.idReserva !== idReserva);
       renderizarReservas();
     })
     .catch((err) => console.error("Error al eliminar:", err));
 }
+
+function editarReserva(){
+  // fetch edit
+}
+
+
+// =============================================== RENDER ==================================================
+function renderizarReservas(){
+  //identifico constantes
+  reservasUsuario.forEach(r =>{
+    // declaro constantes
+    let row = document.getElementById("row-reservas");
+    console.log("boton pillao")
+      row.innerHTML= " "; // limpio
+      row.innerHTML = `
+        <div class="card d-grid">
+          <div class="row">
+            <div class="contenedor-info col-5">
+              <p>Nombre evento : ${r.evento.nombre}</p>
+              <p>Cantidad : ${r.cantidad}</p>
+              <p>Precio Venta : ${r.precioVenta}€</p>
+              <p>Observaciones: ${r.observaciones}</p>
+            </div>
+            <div class="contenedor-botones col-5 d-flex justify-content-center">
+                <button class="me-4 button btn-edit" data-bs-toggle="collapse" data-bs-target="#collapse-editar-${r.idReserva
+        }" aria-expanded="false" aria-controls="collapse-editar-${r.idReserva
+        }">Editar</button>
+                <button class="me-4 button btn-delete" data-id="${r.idReserva
+        }">Eliminar</button>
+            </div>
+            <div class="col-2 seccion-imagen">
+              <img class="imagen-dashboard" src="${r.evento.rutaImagen
+        }" alt="imagen-evento">
+            </div>
+          </div>
+        </div>
+  
+  
+        <div class="collapse mt-2" id="collapse-editar-${r.idReserva}">
+          <div class="card card-body d-flex justify-content-center align-items-center ">
+            <div class="evento-detalles d-flex flex-row align-items-center">
+              <p><strong>Cantidad:</strong>
+                  <input type="text" class="form-control" value="${r.cantidad}">
+                </p>
+                <p><strong>Observaciones:</strong>
+                  <input type="text" class="form-control" value="${r.observaciones}">
+                </p>
+                <button class="btn btn-primary">Guardar cambios</button>
+            </div>
+            
+          </div>
+        </div>
+      `;
+
+      const deleteButton = row.querySelector(".btn-delete");
+      deleteButton.addEventListener("click", () => eliminarReserva(r.idReserva));
+
+      const editButton = row.querySelector("btn-edit");
+      editButton.addEventListener("click",()=>{editarReserva(idReserva)})
+    })
+    
+};
